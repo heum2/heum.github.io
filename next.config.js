00c249d8b2ab97367d3503780b214a0de2902d87
heum2 <jwh6295@gmail.com>
2023-06-18 02:33:41 +0900

    github Action

--- next.config.js
+++ next.config.js
@@ -1,14 +1,13 @@
 const { withContentlayer } = require("next-contentlayer");
 
 const debug = process.env.NODE_ENV !== "production";
-const repository = "heum2.github.io";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
   reactStrictMode: true,
   swcMinify: true,
-  assetPrefix: !debug ? `/${repository}/` : "", // production 일때 prefix 경로
+  assetPrefix: !debug ? "https://heum2.github.io" : "", // production 일때 prefix 경로
   trailingSlash: true, // 빌드 시 폴더 구조 그대로 생성하도록,
 };
 
